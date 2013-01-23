93d017cf1c9ac1b55fab40e6a3e065ba54d55adb
André Walker <andre@andrewalker.net>
2013-01-23 00:21:33 -0200

    Passar por parâmetros variáveis do DFS visit

--- DFS.py
+++ DFS.py
@@ -19,22 +19,18 @@ def DFS(G, s):
 
     for v in G.nodes():
         if cor[v] == 'branco':
-            DFS_VISIT(G,v)
+            visit(G, v, cor, pred, d, f)
 
-def DFS_VISIT(G, s):
-    cor  = {}
-    pred = {}
-    d    = {}
-    f    = {}
-
-    tempo = tempo + 1
-    d[s] = tempo
+def visit(G, s, cor, pred, d, f):
+    tempo  = tempo + 1
+    d[s]   = tempo
     cor[s] = 'cinza'
 
     for v in G[s]:
         if cor[v] == 'branco':
             pred[v] = s
-            DFS_VISIT(G,v)
+            DFS_VISIT(G,v, cor, pred, d, f)
+
     cor[s] = 'preta'
     tempo = tempo + 1
     f[v] = tempo
