4a654d169fa631a6f66ab90587b4718123c00ddc
André Walker <andre@andrewalker.net>
2013-01-23 07:37:51 -0200

    Comentar dijkstra

--- Dijkstra.py
+++ Dijkstra.py
@@ -5,24 +5,26 @@ import numpy as n
 import sys
 
 def Dijkstra(G, s):
-    Lambda = {}
-    Pi = {}
-    Q = G.nodes()
+    Lambda = {}             # valores de "peso" para cada vértice
+    pred = {}               # predecessores
+    Q = G.nodes()           # vértices que ainda não estão na árvore de
+                            # caminhos mínimos
 
+    # inicializamos todos os lambdas com infinito
     for v in G.nodes():
         Lambda[v] = n.inf
 
-    # Caso não haja pesos definidos para os vértices, atribuímos o valor 1.0.
-    # Esta é uma abordagem alternativa à que usamos em Kruskal, de utilizar uma
-    # variável para verificar se estamos levando em conta o peso ou não.
+    # Caso não haja pesos definidos para os vértices, atribuímos o valor 1
     for v1,v2 in G.edges():
         if ('weight' not in G[v1][v2]):
             G[e][x]['weight'] = 1
 
     Lambda[s] = 0
-    Pi[s] = None
+    pred[s] = None
 
     while Q:
+
+        # encontramos o menor valor de Lambda pertencente a Q
         menor = n.inf
         u     = Q[0]
         if sys.version_info[0] < 3:
@@ -36,22 +38,25 @@ def Dijkstra(G, s):
                     menor = v
                     u = k
 
+        # removemos o item de Q, já que está sendo inserido na árvore
         u_index = Q.index(u)
         del Q[u_index]
 
+        # percorremos a vizinhança de u procurando pesos menores
         for v in G[u]:
             if (v in Q) and (Lambda[v] > Lambda[u] + G[u][v]['weight']):
                 Lambda[v] = Lambda[u] + G[u][v]['weight']
-                Pi[v] = u
+                pred[v] = u
 
-    #retorna a arvore de predecessores com a informação de Lambda[v]
-
+    # Criamos um novo grafo vazio do mesmo tipo de G
     H = nx.create_empty_copy(G)
 
+    # Adicionamos os vértices de acordo com os dados de G
     for v1,v2,data in G.edges(data=True):
-        if Pi[v2] is v1:
+        if (pred[v2] is v1) or (pred[v1] is v2 and not nx.is_directed(H)):
             H.add_edge( v1, v2, data )
             H.node[v1]['lambda'] = Lambda[v1]
             H.node[v2]['lambda'] = Lambda[v2]
 
+    # Retornamos a árvore de predecessores com a informação de Lambda[v]
     return H
