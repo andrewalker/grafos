6143d2ac6657d3552cde7d385a915447b23057eb
Camilo Moreira <contato@debugmaster.com.br>
2013-01-23 00:35:32 -0200

    Mudando para Dijsktra do Professor

--- Dijkstra.py
+++ Dijkstra.py
@@ -4,54 +4,25 @@ import networkx as nx
 import numpy as n
 
 def Dijkstra(G, s, t):
-    S = {}
+    Lambda = {}
+    Pi = {}
     Q = G.nodes()
 
     for v in G.nodes():
-        S[v] = n.inf
+        Lambda[v] = n.inf
 
-    S[s] = 0
+    Lambda[s] = 0
+    Pi[s] = None
 
-    backtrack = {}
-
-    for v in G.nodes():
-        backtrack[v] = []
+    # Não preciso do S devido a checagem de v em Q dentro do IF do FOR
 
     while Q:
-        u = min(Q)
+        u = min(Q) # vertice de menor valor do lambda em Q
 
-        if u is t:
-            break
-
-        u_index = Q.index(u)
-        del Q[u_index]
 
         for v in G[u]:
-            if (v in Q) and (S[v] > S[u] + G[u][v]['weight']):
-                S[v] = S[u] + G[u][v]['weight']
-                backtrack[v].append(u)
+            if (v in Q) and (Lambda[v] > Lambda[u] + G[u][v]['weight']):
+                Lambda[v] = Lambda[u] + G[u][v]['weight']
+                Pi[v] = u
 
-    vertices_subgrafo = [ t ]
-    b = backtrack[t].pop()
-
-    while b:
-        vertices_subgrafo.append(b)
-        v = b
-        if backtrack[v]:
-            b = backtrack[v].pop()
-        else:
-            b = 0
-
-    vertices_subgrafo.reverse()
-    caminho = []
-
-    for i in range(len(vertices_subgrafo)-1):
-        caminho.append([ vertices_subgrafo[i], vertices_subgrafo[i+1] ])
-
-    P = G.subgraph(vertices_subgrafo)
-
-   #for aresta in P.edges():
-   #    if aresta not in caminho:
-   #        P.remove_edge(*aresta)
-
-    return P
+    #retorna a arvore de predecessores com a informação de Lambda[v]
