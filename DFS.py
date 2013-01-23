f6595def8980e0c402c0bfcded8d583e8b2d5d52
André Walker <andre@andrewalker.net>
2013-01-23 00:38:20 -0200

    Construir a árvore nova do DFS para retornar

--- DFS.py
+++ DFS.py
@@ -21,6 +21,19 @@ def DFS(G, s):
         if cor[v] == 'branco':
             tempo = visit(G, v, cor, pred, d, f, tempo)
 
+    H = nx.create_empty_copy(G)
+
+    for v2,v1 in pred.interitems():
+        if not H.has_node(v1):
+            H.add_node(v1)
+        if not H.has_node(v2):
+            H.add_node(v2)
+
+        e = G[v1][v2]
+        H.add_edge(*e)
+
+    return H
+
 def visit(G, s, cor, pred, d, f, tempo):
     tempo  = tempo + 1
     d[s]   = tempo
